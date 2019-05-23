ee4186b463d5a8b9f81352b2732a1ca0d2b18ef1
Victor Gomes <victorborgesfg@gmail.com>
2019-05-23 14:46:23 +0100

    prove operations commute, missing never_fails

--- Move_SEC.thy
+++ Move_SEC.thy
@@ -14,14 +14,48 @@ fun valid_move_opers :: "('t, 'n, 'm) state \<Rightarrow> 't \<times>('t, 'n, 'm
 locale move = network_with_constrained_ops _ "interp_op'" "([], {})" valid_move_opers
 begin
 
-
 lemma kleisli_interp_op' [iff]:
   shows \<open>interp_op' (x :: ('t :: {linorder}, 'n, 'm) operation) \<rhd> interp_op' y = interp_op' y \<rhd> interp_op' x\<close>
 proof (unfold kleisli_def, rule ext, clarify)
   fix log :: \<open>('t, 'n, 'm) log_op list\<close> and tree :: \<open>('n \<times> 'm \<times> 'n) set\<close>
-  { assume \<open>unique_parent tree\<close> \<open>distinct (map log_time log @ [move_time x])\<close> \<open>distinct (map log_time log @ [move_time y])\<close>
+  { assume *: \<open>unique_parent tree\<close> \<open>distinct (map log_time log @ [move_time x])\<close> \<open>distinct (map log_time log @ [move_time y])\<close> \<open>move_time x \<noteq> move_time y\<close>
+    obtain logx treex where 1: \<open>interp_op x (log, tree) = (logx, treex)\<close>
+      using * by (clarsimp simp: interp_op'_def)  (metis surj_pair)
+    hence \<open>set (map log_time logx) = {move_time x} \<union> set (map log_time log)\<close>
+      using * by (cases x) (rule interp_op_timestampI2; force)
+    moreover have \<open>distinct (map log_time logx)\<close>
+      using * 1 by (cases x) (rule interp_op_timestampI1; force)
+    ultimately have 2: \<open>distinct (map log_time logx @ [move_time y])\<close>
+      using * by simp
+    obtain logy treey where 3: \<open>interp_op y (log, tree) = (logy, treey)\<close>
+      using * by (clarsimp simp: interp_op'_def)  (metis surj_pair)
+    hence \<open>set (map log_time logy) = {move_time y} \<union> set (map log_time log)\<close>
+      using * by (cases y) (rule interp_op_timestampI2; force)
+    moreover have \<open>distinct (map log_time logy)\<close>
+      using * 3 by (cases y) (rule interp_op_timestampI1, force, force)
+    ultimately have 4: \<open>distinct (map log_time logy @ [move_time x])\<close>
+      using * by simp
+    have \<open>unique_parent treex\<close> \<open>unique_parent treey\<close>
+      using * 1 3 interp_op_unique_parent by blast+
+    hence \<open>interp_op' x (log, tree) \<bind> interp_op' y = interp_op' y (log, tree) \<bind> interp_op' x\<close>
+      using * 1 2 3 4 by (cases x, cases y, clarsimp simp: interp_op'_def) (rule interp_op_commute2I; force)
+  }
+  moreover {
+    assume *: \<open>unique_parent tree\<close> \<open>distinct (map log_time log @ [move_time x])\<close> \<open>distinct (map log_time log @ [move_time y])\<close> \<open>move_time x = move_time y\<close>
+    obtain logx treex where 1: \<open>interp_op x (log, tree) = (logx, treex)\<close>
+      using * by (clarsimp simp: interp_op'_def)  (metis surj_pair)
+    hence \<open>set (map log_time logx) = {move_time x} \<union> set (map log_time log)\<close>
+      using * by (cases x) (rule interp_op_timestampI2; force)
+    hence 2: \<open>\<not> distinct (map log_time logx @ [move_time y])\<close>
+      using * by simp
+    obtain logy treey where 3: \<open>interp_op y (log, tree) = (logy, treey)\<close>
+      using * by (clarsimp simp: interp_op'_def)  (metis surj_pair)
+    hence \<open> set (map log_time logy) = {move_time y} \<union> set (map log_time log)\<close>
+      using * by (cases y) (rule interp_op_timestampI2; force)
+    hence 4: \<open>\<not> distinct (map log_time logy @ [move_time x])\<close>
+      using * by simp
     have \<open>interp_op' x (log, tree) \<bind> interp_op' y = interp_op' y (log, tree) \<bind> interp_op' x\<close>
-      sorry
+      using * 1 2 3 4 by (clarsimp simp: interp_op'_def)
   }
   moreover {
     assume *: \<open>unique_parent tree\<close> \<open>\<not> distinct (map log_time log @ [move_time x])\<close> \<open>distinct (map log_time log @ [move_time y])\<close>
@@ -54,124 +88,52 @@ proof (unfold kleisli_def, rule ext, clarify)
       using * by (clarsimp simp: interp_op'_def)
   }
   ultimately show \<open>interp_op' x (log, tree) \<bind> interp_op' y = interp_op' y (log, tree) \<bind> interp_op' x\<close>
-    by (clarsimp simp: interp_op'_def)
-
+    by (clarsimp simp: interp_op'_def) fastforce
 qed
-(*
-  apply(rule ext)
-  apply clarify
-  apply (case_tac \<open>unique_parent b \<and> distinct (map log_time a)\<close>)
-   prefer 2
-   apply force
-  apply (case_tac \<open>distinct (map log_time a @ [move_time x, move_time y])\<close>)
-   apply clarify
-   apply (case_tac \<open>interp_op x (a, b)\<close>)
-   apply (case_tac \<open>interp_op y (a, b)\<close>)
-   apply (subgoal_tac \<open>unique_parent ba \<and> unique_parent bb\<close>)
-  prefer 2
-  using interp_op_unique_parent apply blast
-  apply clarsimp
-  apply (subgoal_tac \<open>distinct (map log_time aa) \<and>
-                      distinct (map log_time ab)\<close>)
-    prefer 2
-    apply (rule conjI)
-     apply (cases x)
-     apply clarsimp
-      apply (rule interp_op_timestampI1)
-       prefer 2
-       apply assumption
-     apply force
-     apply (cases y)
-     apply clarsimp
-      apply (rule interp_op_timestampI1)
-       prefer 2
-       apply assumption
-    apply force
-   apply clarsimp
-   apply (case_tac \<open>move_time x \<notin> log_time ` set ab \<and> move_time y \<notin> log_time ` set aa\<close>)
-    apply clarsimp
-    apply (cases x, cases y)
-  apply clarsimp
-    apply (rule interp_op_commute2I)
-       apply force
-      apply force
-     apply force
-    apply force
   
-
-*)
-
-(*
-lemma timestamps_distinct:
-  assumes \<open>xs prefix of i\<close>
-  and \<open>ops = node_deliver_messages xs\<close>
-shows \<open>distinct (map (move_time \<circ> snd) ops)\<close>
-  sorry*)
-
-lemma kleisli_interp_op' [iff]:
-  shows \<open>interp_op' x \<rhd> interp_op' y = interp_op' y \<rhd> interp_op' x\<close>  
-  apply(unfold interp_op'_def kleisli_def)
-  apply(rule ext)
-  apply clarify
-  apply (case_tac \<open>unique_parent b \<and> distinct (map log_time a)\<close>)
-   prefer 2
-   apply force
-  apply (case_tac \<open>distinct (map log_time a @ [move_time x, move_time y])\<close>)
-   apply clarify
-   apply (case_tac \<open>interp_op x (a, b)\<close>)
-   apply (case_tac \<open>interp_op y (a, b)\<close>)
-   apply (subgoal_tac \<open>unique_parent ba \<and> unique_parent bb\<close>)
-  prefer 2
-  using interp_op_unique_parent apply blast
-  apply clarsimp
-  apply (subgoal_tac \<open>distinct (map log_time aa) \<and>
-                      distinct (map log_time ab)\<close>)
-    prefer 2
-    apply (rule conjI)
-     apply (cases x)
-     apply clarsimp
-      apply (rule interp_op_timestampI1)
-       prefer 2
-       apply assumption
-     apply force
-     apply (cases y)
-     apply clarsimp
-      apply (rule interp_op_timestampI1)
-       prefer 2
-       apply assumption
-    apply force
-   apply clarsimp
-   apply (case_tac \<open>move_time x \<notin> log_time ` set ab \<and> move_time y \<notin> log_time ` set aa\<close>)
-    apply clarsimp
-    apply (cases x, cases y)
-  apply clarsimp
-    apply (rule interp_op_commute2I)
-       apply force
-      apply force
-     apply force
-    apply force
-  
-
-
-  
-
 lemma concurrent_operations_commute:
   assumes \<open>xs prefix of i\<close>
   shows \<open>hb.concurrent_ops_commute (node_deliver_messages xs)\<close>
-  using assms
-  apply(clarsimp simp add: hb.concurrent_ops_commute_def)
-  apply(unfold interp_msg_def; simp)
-  prefer 2
-  
-  done
+  using assms by (clarsimp simp add: hb.concurrent_ops_commute_def) (unfold interp_msg_def; simp)
+
+lemma apply_operations_never_fails:
+  assumes "xs prefix of i"
+  shows "apply_operations xs \<noteq> None"
+  sorry
 
 sublocale sec: strong_eventual_consistency weak_hb hb interp_msg
   "\<lambda>os. \<exists>xs i. xs prefix of i \<and> node_deliver_messages xs = os" "([], {})"
-  apply(standard; clarsimp)
-      apply(auto simp add: hb_consistent_prefix drop_last_message node_deliver_messages_distinct concurrent_operations_commute)
-  apply(clarsimp simp add: interp_msg_def interp_op'_def)
-  using drop_last_message apply blast
-  done
+proof (standard; clarsimp)
+  fix xsa i
+  assume "xsa prefix of i"
+  thus "hb.hb_consistent (node_deliver_messages xsa)"
+    by(auto simp add: hb_consistent_prefix)
+next
+  fix xsa i
+  assume "xsa prefix of i"
+  thus "distinct (node_deliver_messages xsa)"
+    by(auto simp add: node_deliver_messages_distinct)
+next
+  fix xsa i
+  assume "xsa prefix of i"
+  thus "hb.concurrent_ops_commute (node_deliver_messages xsa)"
+    by(auto simp add: concurrent_operations_commute)
+next
+  fix xs a b state xsa x
+  assume "hb.apply_operations xs ([], {}) = Some state"
+         "node_deliver_messages xsa = xs @ [(a, b)]"
+         "xsa prefix of x"
+  moreover hence "apply_operations xsa \<noteq> None"
+    using apply_operations_never_fails by blast
+  ultimately show "\<exists>ab bb. interp_msg (a, b) state = Some (ab, bb)"
+    by (clarsimp simp: apply_operations_def kleisli_def)
+next
+  fix xs a b xsa x
+  assume "node_deliver_messages xsa = xs @ [(a, b)]"
+    and "xsa prefix of x"
+  thus "\<exists>xsa. (\<exists>x. xsa prefix of x) \<and> node_deliver_messages xsa = xs"
+    using drop_last_message by blast
+qed
 
 end
 
