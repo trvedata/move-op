c320779cc0f15a63c112997d665a91a70bf3958e
Martin Kleppmann <martin@kleppmann.com>
2019-06-12 17:03:54 +0100

    Fix generating integer values

--- evaluation/src/main/scala/Main.scala
+++ evaluation/src/main/scala/Main.scala
@@ -1,6 +1,8 @@
 object Main extends App {
+  def int(value: Int): generated.int = generated.int_of_integer(BigInt(value))
+
   val initState = (Nil, generated.hm_empty[String, (String, String)].apply(()))
-  val operation = generated.Move((generated.zero_int, "a"), "parent", "meta", "child")
+  val operation = generated.Move((int(0), "a"), "parent", "meta", "child")
   val state = generated.example_apply_op(operation)(initState)
   println(state)
 }
