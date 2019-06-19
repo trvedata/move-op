7c30938d0031d25dca97081c2aeee28dba56ff0a
Martin Kleppmann <martin@kleppmann.com>
2019-06-19 20:48:17 +0100

    evaluation: profiling of remote operation application
    Result: 88% of the time is spent in hm_update; 85% spent in
    rbt_insert_with_key (rebalancing of red-black tree?). Doesn't look like
    it will be easy to speed this up.
    
    Only 8% of the time is spent in hm_lookup, in contrast to applying local
    operations, for which hm_lookup is the dominant cost (68%). But since
    local operations are vastly faster anyway, hm_lookup doesn't really
    matter.

--- evaluation/build.sbt
+++ evaluation/build.sbt
@@ -5,6 +5,9 @@ version := "1.0"
 libraryDependencies += "io.dropwizard.metrics" % "metrics-core" % "4.1.0"
 libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.7.26"
 
+// Uncomment this to enable profiling of application run with 'sbt run'
+//javaOptions in run += "-agentpath:/usr/local/YourKit-JavaProfiler-2019.1/bin/linux-x86-64/libyjpagent.so=port=10001,listen=all"
+
 enablePlugins(AkkaGrpcPlugin)
 // ALPN agent
 enablePlugins(JavaAgent)
--- evaluation/src/main/scala/Main.scala
+++ evaluation/src/main/scala/Main.scala
@@ -1,19 +1,32 @@
 import java.util.Random
 
 object Main extends App {
+  val random = new Random()
+
   def int(value: Int): generated.int = generated.int_of_integer(BigInt(value))
+
+  def operations(num: Int, actorId: String) = {
+    (0 to num).map { i =>
+      val parent = "%d".format(random.nextInt(1000))
+      val child  = "%d".format(random.nextInt(1000))
+      generated.Move((int(i), actorId), parent, "", child)
+    }
+  }
 
   var state: (List[generated.log_op[(generated.int, String),String,String]], generated.hashmap[String,(String, String)])
     = (Nil, generated.hm_empty[String, (String, String)].apply(()))
-  val random = new Random()
-  val startTime = System.nanoTime()
 
-  for (i <- 1 to 1000) {
-    val parent = "%d".format(random.nextInt(1000))
-    val child  = "%d".format(random.nextInt(1000))
-    val operation = generated.Move((int(i), "a"), parent, "", child)
-    state = generated.example_apply_op(operation)(state)
+  println("applying local ops...")
+  val start1 = System.nanoTime()
+  for (op <- operations(1000, "a")) {
+    state = generated.example_apply_op(op)(state)
   }
+  println("elapsed time: %d ms" format ((System.nanoTime() - start1) / 1000000))
 
-  println("elapsed time: %d ms" format ((System.nanoTime() - startTime) / 1000000))
+  println("applying remote ops...")
+  val start2 = System.nanoTime()
+  for (op <- operations(1000, "b")) {
+    state = generated.example_apply_op(op)(state)
+  }
+  println("elapsed time: %d ms" format ((System.nanoTime() - start2) / 1000000))
 }
