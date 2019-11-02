ad16997dc8dcc2a47dffdf9a21e66c1cdd9c6845
Martin Kleppmann <martin@kleppmann.com>
2019-11-02 17:42:01 +0000

    VM setup instructions

--- evaluation/run-all.sh
+++ evaluation/run-all.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 set -e
 
+# To run the experiments, log in to the AWS EC2 console and start up a c5.large
+# instance running Ubuntu 18.04 in each of the three regions us-west-1, eu-west-1,
+# and ap-southeast-1. Then fill in their IP addresses here:
 US_WEST_1=18.144.1.184
 EU_WEST_1=34.255.190.108
 AP_SOUTHEAST_1=18.140.64.126
+
+# Set up the security groups such that you can log in to the VMs by SSH (TCP port 22),
+# and that they can all connect to each other on TCP port 8080.
+# Then, to set up the VMs, log in to each by SSH and run the following:
+
+# sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y apt-transport-https gnupg wget unzip
+# echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+# sudo apt-get update && sudo apt-get install -y openjdk-8-jdk-headless sbt
+# git clone https://github.com/trvedata/move-op.git && cd move-op/evaluation && sbt compile
 
 run_batch () {
     INTERVAL=$1
