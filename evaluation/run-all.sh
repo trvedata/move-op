52a5da46cbb875292a0b6bc9a8f689a4744e7072
Martin Kleppmann <martin@kleppmann.com>
2020-02-26 15:53:54 +0000

    Document evaluation procedure

--- evaluation/run-all.sh
+++ evaluation/run-all.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
+# Run this script with one argument: the interval between generated move
+# operations, in milliseconds, on each replica.
+INTERVAL="${1?Please specify interval between operations (in milliseconds)}"
+
 # To run the experiments, log in to the AWS EC2 console and start up a c5.large
 # instance running Ubuntu 18.04 in each of the three regions us-west-1, eu-west-1,
 # and ap-southeast-1. Then fill in their IP addresses here:
-US_WEST_1=18.144.1.184
-EU_WEST_1=34.255.190.108
-AP_SOUTHEAST_1=18.140.64.126
+US_WEST_1=13.52.240.53
+EU_WEST_1=52.48.156.251
+AP_SOUTHEAST_1=54.169.188.185
 
 # Set up the security groups such that you can log in to the VMs by SSH (TCP port 22),
 # and that they can all connect to each other on TCP port 8080.
@@ -18,21 +22,21 @@ AP_SOUTHEAST_1=18.140.64.126
 # sudo apt-get update && sudo apt-get install -y openjdk-8-jdk-headless sbt
 # git clone https://github.com/trvedata/move-op.git && cd move-op/evaluation && sbt compile
 
-run_batch () {
-    INTERVAL=$1
-
-    ssh -i ~/.ec2/martin-aws-us-west-1.pem ubuntu@$US_WEST_1 /home/ubuntu/move-op/evaluation/run-test.sh $INTERVAL 1 $EU_WEST_1 $AP_SOUTHEAST_1 &
-    ssh -i ~/.ec2/martin-aws-eu-west-1.pem ubuntu@$EU_WEST_1 /home/ubuntu/move-op/evaluation/run-test.sh $INTERVAL 2 $US_WEST_1 $AP_SOUTHEAST_1 &
-    ssh -i ~/.ec2/martin-aws-ap-southeast-1.pem ubuntu@$AP_SOUTHEAST_1 /home/ubuntu/move-op/evaluation/run-test.sh $INTERVAL 3 $US_WEST_1 $EU_WEST_1 &
-    wait
-
-    scp -i ~/.ec2/martin-aws-us-west-1.pem ubuntu@$US_WEST_1:/home/ubuntu/move-op/evaluation/data/logs/interval_$INTERVAL.log data/logs/interval_${INTERVAL}_us_west_1.log
-    scp -i ~/.ec2/martin-aws-eu-west-1.pem ubuntu@$EU_WEST_1:/home/ubuntu/move-op/evaluation/data/logs/interval_$INTERVAL.log data/logs/interval_${INTERVAL}_eu_west_1.log
-    scp -i ~/.ec2/martin-aws-ap-southeast-1.pem ubuntu@$AP_SOUTHEAST_1:/home/ubuntu/move-op/evaluation/data/logs/interval_$INTERVAL.log data/logs/interval_${INTERVAL}_ap_southeast_1.log
-
-    gzip data/logs/interval_${INTERVAL}_us_west_1.log data/logs/interval_${INTERVAL}_eu_west_1.log data/logs/interval_${INTERVAL}_ap_southeast_1.log
-}
+# Once that setup is done, the following commands run the test script on the
+# three instances concurrently, and then copy the log files off the instances
+# into evaluation/data/logs/*.log.gz. Those logs are then analysed by the
+# script evaluation/data/processing_times.sh.
 
 cd "$(dirname "$0")"
 mkdir -p data/logs
-run_batch 14
+
+ssh -i ~/.ec2/martin-aws-us-west-1.pem ubuntu@$US_WEST_1 /home/ubuntu/move-op/evaluation/run-test.sh $INTERVAL 1 $EU_WEST_1 $AP_SOUTHEAST_1 &
+ssh -i ~/.ec2/martin-aws-eu-west-1.pem ubuntu@$EU_WEST_1 /home/ubuntu/move-op/evaluation/run-test.sh $INTERVAL 2 $US_WEST_1 $AP_SOUTHEAST_1 &
+ssh -i ~/.ec2/martin-aws-ap-southeast-1.pem ubuntu@$AP_SOUTHEAST_1 /home/ubuntu/move-op/evaluation/run-test.sh $INTERVAL 3 $US_WEST_1 $EU_WEST_1 &
+wait
+
+scp -i ~/.ec2/martin-aws-us-west-1.pem ubuntu@$US_WEST_1:/home/ubuntu/move-op/evaluation/data/logs/interval_$INTERVAL.log data/logs/interval_${INTERVAL}_us_west_1.log
+scp -i ~/.ec2/martin-aws-eu-west-1.pem ubuntu@$EU_WEST_1:/home/ubuntu/move-op/evaluation/data/logs/interval_$INTERVAL.log data/logs/interval_${INTERVAL}_eu_west_1.log
+scp -i ~/.ec2/martin-aws-ap-southeast-1.pem ubuntu@$AP_SOUTHEAST_1:/home/ubuntu/move-op/evaluation/data/logs/interval_$INTERVAL.log data/logs/interval_${INTERVAL}_ap_southeast_1.log
+
+gzip data/logs/interval_${INTERVAL}_us_west_1.log data/logs/interval_${INTERVAL}_eu_west_1.log data/logs/interval_${INTERVAL}_ap_southeast_1.log
